6da1e7f3dc0c35cf91ebb7b589b96c4e14e88066
Sergio Freitas <sergio.sgfreitas@gmail.com>
2016-06-17 15:38:02 -0300

    creating

---
+++ src/app.js
@@ -0,0 +1,12 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'pages/welcome',      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: 'pages/users',        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: 'pages/child-router', nav: true, title: 'Child Router' }
+    ]);
+
+    this.router = router;
+  }
+}
